67f84fcd4b206bbb6bce8774ea353a647b1b48ff
Myrtilli <kopt7877@gmail.com>
2025-12-15 16:50:14 +0200

    short url now can be null

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -2,7 +2,7 @@
 CREATE TABLE short_urls(
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     long_url TEXT NOT NULL,
-    short_url TEXT UNIQUE NOT NULL
+    short_url TEXT UNIQUE
 );
 
 CREATE INDEX short_url_idx on short_urls(short_url);
